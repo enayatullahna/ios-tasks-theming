6c4100ac10b73f0d5343aa83d6cd38ff0c4b69bf
Enayatullah Naseri <ena.nas@icloud.com>
2019-06-19 18:53:53 -0700

    Started project

---
+++ Tasks/Tasks/Helper/ApperanceHelper.swift
@@ -0,0 +1,17 @@
+//
+//  ApperanceHelper.swift
+//  Tasks
+//
+//  Created by Enayatullah Naseri on 6/19/19.
+//  Copyright © 2019 Andrew R Madsen. All rights reserved.
+//
+
+import UIKit
+
+enum ApperanceHelper {
+    
+    static var midPurple = UIColor(red: <#T##CGFloat#>, green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: <#T##CGFloat#>)
+    
+    
+    
+}
